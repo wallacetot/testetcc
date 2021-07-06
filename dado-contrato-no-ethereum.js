9f86d498fdbd84227ffc39fcec56befd99b525d5
wallacetot <84424187+wallacetot@users.noreply.github.com>
2021-07-06 01:26:09 -0300

    Add files via upload

---
+++ dado-contrato-no-ethereum.js
@@ -0,0 +1,92 @@
+const contractAddress = "0x2D08cE4fDE65625dd85320a76718B131A51F2EF7";
+var smartContract;
+var smartContractWithSigner;
+//Both ways are valid
+const ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "addMarcaCarro",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "addModeloCarro",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "addCorCarro",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "addVelocidadeMaxima",
+				"type": "uint256"
+			}
+		],
+		"name": "registerCarros",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "carros",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "marcaCarro",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "modeloCarro",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "corCarro",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "velocidadeMaxima",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
---
+++ funcoes-contrato-ethereum.js
@@ -0,0 +1,58 @@
+async function getDadosDosCarros() {
+    let contractNumberField = document.getElementById("contractNumber");
+    const userInput = contractNumberField.value * 1;
+    alert("valorInformadoPeloUsuario: " + userInput);
+    try {
+      const arrayDadosDosCarros = await smartContract.carros(userInput);
+      console.log(arrayDadosDosCarros);
+            //Modo sintetico onde se concatena um comando junto a outro na mesma linha
+      document.getElementById("marcaCarro").innerText = arrayDadosDosCarros[1];
+      document.getElementById("modeloCarro").innerText = arrayDadosDosCarros[2];
+      document.getElementById("corCarro").innerText = arrayDadosDosCarros[3];
+      document.getElementById("velocidadeMaxima").innerText = arrayDadosDosCarros[4];
+    } catch (err) {
+      console.error(err);
+      document.getElementById("marcaCarro").innerText = "";
+      document.getElementById("modeloCarro").innerText = "";
+      document.getElementById("corCarro").innerText = "";
+      document.getElementById("velocidadeMaxima").innerText = "";
+      contractNumberField.value = 0;
+      alert("Houve um erro ao buscar o carro nº: " + userInput);
+    }
+  }
+  
+  async function autoLoadOwner() {
+    try {
+      const contractOwner = await smartContract.owner();
+      console.log(contractOwner);
+      document.getElementById("spanOwner").innerText = contractOwner;
+    } catch (err) {
+      console.error(err);
+      alert("Houve um erro ao buscar o proprietário do contrato");
+    }
+  }
+  
+  async function saveFormData() {
+    try {
+      var tx;
+      var txReceipt;
+      tx = await smartContractWithSigner.registerCarros(
+        document.frmCarros.addMarcaCarro.value,
+        document.frmCarros.addModeloCarro.value,
+        document.frmCarros.addCorCarro.value,
+        document.frmCarros.addVelocidadeMaxima.value
+      );
+      console.log("transacao enviada ao metamask. pendente...", tx);
+      alert("Transação enviada... " + tx.hash + " aguarde a confirmação da Blockcnain...");
+      txReceipt = await tx.wait();
+      console.log("transacao processada...", txReceipt);
+      if (txReceipt.status == 1) {
+        alert("Transação processada: " + tx.hash + "  - Registro salvo na Blockchain. Status: " + txReceipt.status);
+      } else {
+        alert("Transação processada: " + tx.hash + "  - Mas houve um erro na blockchain. Veja pelo etherscan");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Houve um erro ao salvar o registro do contrato do novo carro");
+    }
+  }
